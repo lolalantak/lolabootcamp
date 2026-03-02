af34fe2b2e4bdd1aec360e3403fb3fff833ffbeb
lolantak <alzarib24@gmail.com>
2026-03-02 12:03:37 +0800

    first commit

---
+++ py/01_variablesdatatype.py
@@ -0,0 +1,39 @@
+# Topic 01 Variables and Data Types
+
+name = "Shah"   # String
+age = 43        # integer
+weight = 80     # Float
+is_male = True  # Boolean (true or false statement)
+
+her_name = "Lola"
+melaka  = "State"
+
+
+print (name)
+print (type (name))
+
+
+x = 10
+y = 3
+
+print (x+y) # Addition
+print (x-y) # Subtraction
+print (x*y) # Multiplication
+print (x/y) # Division
+print (x // y) # Floor division
+print (x%y) # Modulus
+print (x ** y) # Exponentiation
+
+
+
+
+
+# Exercise 2
+
+c = 25
+f = c * 9/5 + 32
+
+print (f)
+
+
+
